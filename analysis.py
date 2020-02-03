a43116fa5b59528183caa9795e7f6c8b995e0d58
subendhu19 <subendhu.iitm@gmail.com>
2020-02-03 11:34:17 -0500

    Minor fix

--- analysis.py
+++ analysis.py
@@ -1,10 +1,14 @@
 import torch
+import json
 from run_snli import BertForSequenceClassificationWithSparsity, SparseBertForSequenceClassification
 from transformers import BertTokenizer
 
 
 def main():
     config = 1
+
+    global sparse_config
+    sparse_config = json.load(open('configs/config{}.json'.format(str(config))))
     pretrained_folder = '/mnt/nfs/scratch1/srongali/sparsification/exps/config_{}/checkpoint-15000'.format(str(config))
     if config <= 32:
         model = BertForSequenceClassificationWithSparsity.from_pretrained(pretrained_folder)
