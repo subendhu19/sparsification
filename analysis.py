1c627c7b19e14852dde854d40ce23c4a1bddeaaf
subendhu19 <subendhu.iitm@gmail.com>
2020-02-03 11:59:41 -0500

    Fixes

--- analysis.py
+++ analysis.py
@@ -211,13 +211,16 @@ def main():
 
     global sparse_config
     sparse_config = json.load(open('configs/config{}.json'.format(str(config))))
-    pretrained_folder = '/mnt/nfs/scratch1/srongali/sparsification/exps/config_{}/checkpoint-15000'.format(str(config))
+    pretrained_folder = '/mnt/nfs/scratch1/srongali/sparsification/snli_exps/config_{}/checkpoint-15000'.format(str(config))
     if config <= 32:
         model = BertForSequenceClassificationWithSparsity.from_pretrained(pretrained_folder)
     else:
         model = SparseBertForSequenceClassification.from_pretrained(pretrained_folder)
+    model.eval()
 
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+    # Get the actual dataset or examples
     input_ids = torch.tensor([tokenizer.encode('this is it'), tokenizer.encode('this is not')])
 
     print(model.get_sparse_embeddings(input_ids))
